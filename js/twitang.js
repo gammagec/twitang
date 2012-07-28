62ad5ae8ba32bad62d4e6c9f291ca80483761265
Christopher Gammage <gammagec@gmail.com>
2012-07-28 01:01:25 -0700

    not sure

--- js/twitang.js
+++ js/twitang.js
@@ -1,19 +1,36 @@
-angular.module('twitang', []).
+angular.module('twitfetcher', ['ngResource']).
+    factory('TweetFetcher', function($resource) {
+        var TweetFetcher = $resource('http://api.twitter.com/1/statuses/user_timeline.json',
+            {
+                include_rts: 'true',
+                count: 4,
+                include_entities: 'true'
+            }
+        );
+        TweetFetcher.prototype.fetch = function(screenName, cb) {
+            return TweetFetcher.get({screen_name: screenName}, cb);
+        }
+        return TweetFetcher;
+    });
+
+angular.module('twitang', ['twitfetcher']).
     directive('twitterProfile', function() {
-	return {
-	    restrict: 'E',
-	    transclude: true,	    
-	    scope: { screenName: '@' },
-	    templateUrl: 'twitterProfile.html',
-	    replace: true,
-	    controller: function($scope) {
-		var headerHeight = 52;
-		var footerHeight = 41;
-		$scope.profileImage = "https://si0.twimg.com/profile_images/2250905904/7169356034_d0e2e23be0_m_normal.png";
-		$scope.userName = "Christopher Gammage";
-		$scope.height = 800;
-		$scope.width = 300;
-		$scope.bodyHeight = $scope.height - headerHeight - footerHeight;
-	    }
-	}; 
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: { screenName: '@' },
+            templateUrl: 'twitterProfile.html',
+            replace: true,
+            controller: function($scope, $attrs, TweetFetcher) {
+                var headerHeight = 52;
+                var footerHeight = 41;
+                $scope.profileImage = null;
+                $scope.userName = "";
+                $scope.height = 800;
+                $scope.width = 300;
+                $scope.bodyHeight = $scope.height - headerHeight - footerHeight;
+                TweetFetcher.fetch($attrs.screenName, function() {
+                });
+            }
+        };
     });
